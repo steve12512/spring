ba02ee47e0bfdd0eba6c95530dfffb601e976933
steve12512 <86954812+steve12512@users.noreply.github.com>
2026-02-08 18:12:00 +0200

    adds tests

---
+++ demo/src/test/java/com/example/demo/service/UserServiceTest.java
@@ -0,0 +1,51 @@
+package com.example.demo.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import com.example.demo.domain.User;
+import com.example.demo.dto.responses.user_responses.UserResponse;
+import com.example.demo.exception.UserAlreadyExistsException;
+import com.example.demo.repository.user.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+  @Mock private UserRepository userRepository;
+
+  @InjectMocks private UserService userService;
+
+  @Test
+  public void testCreateUser() {
+    User user = new User("steve", "stevekalelis@hotmail.com", 26, true);
+
+    when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
+    when(userRepository.save(any(User.class))).thenReturn(user);
+
+    UserResponse mockResponse =
+        userService.createUser(user.getAge(), user.getUsername(), user.getEmail());
+
+    assertNotNull(mockResponse);
+    assertEquals(user.getUsername(), mockResponse.getUsername());
+
+    verify(userRepository).save(any(User.class));
+  }
+
+  @Test
+  public void testCreateUserThrowsUserAlreadyExistsException() {
+    User user = new User("steve", "stevekalelis@hotmail.com", 26, true);
+
+    when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+
+    assertThrows(
+        UserAlreadyExistsException.class,
+        () -> userService.createUser(user.getAge(), user.getUsername(), user.getEmail()));
+  }
+}
